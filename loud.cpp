aff1c2b9bc7e75d2a250498e8853ddd756aac7fa
matias robles <matias.robles1601@gmail.com>
2021-02-19 12:18:25 -0300

    subordinados listo!

--- loud.cpp
+++ loud.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 using namespace sdsl;
 
+//Clase Persona (PARA ORGANIGRAMA)
 class Persona
 {
 private:
@@ -21,6 +22,74 @@ public:
  const string &getCargo() const { return cargo; }
 };
 
+//IMPLEMENTACIÓN DE LOUD CON BIT_VECTOR DE SDSL
+
+/*First Child, este método devuelve un entero que representa el índice dentro del bit_vector con el primer hijo del un nodo dado, 
+de no poseer un hijo el método retorna -1.*/
+int first_child(bit_vector b, int i)
+{
+ rank_support_v<1> b_rank(&b);
+ bit_vector::select_0_type b_sel(&b);
+ int rank = b_rank(i);
+ int firstChild;
+ if (rank == 0)
+ {
+		firstChild = b_sel(1) + 1;
+ }
+ else
+ {
+		firstChild = (b_sel(b_rank(i + 1)) + 1);
+ }
+ if (b[firstChild] == 1)
+ {
+		return firstChild;
+ }
+ return -1;
+}
+
+/*Next Sibling, este método devuelve un entero que representa el indice dentro del bit_vector con el siguiente nodo hermano de un nodo dado,
+de no poseer uno o más hermanos, el método retorna -1*/
+int next_sibling(bit_vector b, int i)
+{
+ int next = b[i + 1];
+ if (next == 1)
+ {
+		return (i + 1);
+ }
+ return -1;
+}
+
+/*Parent, este método devulve un entero que representa el indice dentro del bit_vector con el padre de un nodo dado,
+al ser nodo raiz el método retorna -1*/
+int parent(bit_vector b, int i)
+{
+ rank_support_v<0> b_rank(&b);
+ bit_vector::select_1_type b_sel(&b);
+ if (b_rank(i) == 0)
+ {
+		return -1;
+ }
+ int parent = b_sel(b_rank(i));
+ return parent;
+}
+
+//Child, este método devuelve un entero que representa el i-esimo hijo de un determinado nodo
+int child(bit_vector b, int p, int i)
+{
+ rank_support_v<1> b_rank(&b);
+ bit_vector::select_0_type b_sel(&b);
+ int i_child = b_sel(b_rank(p + i - 1)) + i;
+ return i_child;
+}
+
+//Data, este método es básicamente un toString a partir de un indice de referencia dentro del bit_vector
+void data(bit_vector b, vector<Persona> personas, int i)
+{
+ rank_support_v<1> b_rank(&b);
+ cout << " id     : " << b_rank(i) << "\n nombre : " << personas[b_rank(i)].getName() << "\n cargo  : " << personas[b_rank(i)].getCargo() << endl;
+}
+
+//Método para creación de bit_vector códificado en unario basado en "1"
 bit_vector encoder_unario(vector<Persona> personas, string children)
 {
  int sum = 0;
@@ -40,6 +109,7 @@ bit_vector encoder_unario(vector<Persona> personas, string children)
  return out;
 }
 
+//Método para creación de Organigrama
 vector<Persona> addEmpleado(vector<Persona> personas, string *children, string nombrePadre)
 {
  string nombre;
@@ -82,33 +152,6 @@ vector<Persona> addEmpleado(vector<Persona> personas, string *children, string n
  return personas;
 }
 
-int first_child(bit_vector b, int i)
-{
- rank_support_v<1> b_rank(&b);
- bit_vector::select_0_type b_sel(&b);
- int firstChild = (b_sel(b_rank(i)) + 1);
- return firstChild;
-}
-
-int parent(bit_vector b, int i)
-{
- rank_support_v<0> b_rank(&b);
- bit_vector::select_0_type b_sel(&b);
- bit_vector::select_1_type b_sel1(&b);
- int parent = (b_sel(b_rank(b_sel1(b_rank(i)))) + 1);
- return parent;
-}
-
-int next_sibling(bit_vector b, int i)
-{
- int next = b[i + 1];
- if (next == 1)
- {
-		return (i + 1);
- }
- return 0;
-}
-
 int main()
 {
 
@@ -125,18 +168,66 @@ int main()
 
  for (int i = 0; i < personas.size(); i++)
  {
-		cout << " nombre : " << personas[i].getName() << "\n cargo  : " << personas[i].getCargo() << "\n=======================================" << endl;
+		cout << " id     : " << (i + 1) << "\n nombre : " << personas[i].getName() << "\n cargo  : " << personas[i].getCargo() << "\n=======================================" << endl;
  }
  bit_vector unario = encoder_unario(personas, children);
- cout << "LOUD : " << unario << endl;
- int fistc = first_child(unario, 1);
- cout << "first Child : " << fistc << endl;
- cout << "LOUD[First Child] : " << unario[fistc] << endl;
- int siblin = next_sibling(unario, 2);
- cout << "nextSibling : " << siblin << endl;
- cout << "LOUD[siblin] : " << unario[siblin] << endl;
- // int parentS = parent(unario, 3);
- // cout << "parent : " << parentS << endl;
- // cout << "LOUD[parent] : " << unario[parentS] << endl;
+ rank_support_v<1> b_rank(&unario);
+ bool salir = false;
+ int sal;
+ int padre = 0;
+ int hijo = -1;
+ while (!salir)
+ {
+		cout << "========== MENU =========" << endl;
+		cout << "= 1) Subordinados       =" << endl;
+		cout << "= 2) Jefe               =" << endl;
+		cout << "= 3) Colegas            =" << endl;
+		cout << "= 4) Cadena de mando    =" << endl;
+		cout << "= 5) Buscar Nodo        =" << endl;
+		cout << "= 6) Imprimir Árbol     =" << endl;
+		cout << "= 7) Salir              =" << endl;
+		cout << "=========================" << endl;
+		cin >> sal;
+		switch (sal)
+		{
+		case 1:
+			cout << "Indique el nodo del cual desea saber sus subordinados: ";
+			cin >> padre;
+			padre = b_rank(padre + 1);
+			cout << padre << "adsjksdjfalksjdflasjdfl" << endl;
+			hijo = first_child(unario, padre);
+			cout << "\n========= Lista de Subordinados ==========\n"
+								<< endl;
+			if (hijo == -1)
+			{
+				cout << "ESTE TRABAJADOR NO POSEE SUBORDINADOS\n";
+				cout << "\n=========================================\n"
+									<< endl;
+			}
+			while (hijo > -1)
+			{
+				data(unario, personas, hijo);
+				cout << "\n==========================================\n"
+									<< endl;
+				hijo = next_sibling(unario, hijo);
+			}
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+		case 7:
+			salir = !salir;
+			break;
+		deaful:
+			break;
+		}
+ }
  return 0;
 }
